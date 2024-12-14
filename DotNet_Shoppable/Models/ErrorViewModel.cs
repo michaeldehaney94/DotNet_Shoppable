bb9b127f73a757506c5410430d4e55b2fb5c27c9
Michael Dehaney <dehaney21@gmail.com>
2024-12-14 14:37:22 -0500

    new files

---
+++ DotNet_Shoppable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet_Shoppable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
