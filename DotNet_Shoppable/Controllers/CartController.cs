1119355ac2dfa24002fc4eab3fa179483ce6effd
Michael Dehaney <dehaney21@gmail.com>
2025-01-12 15:38:21 -0500

    new update

--- DotNet_Shoppable/Controllers/CartController.cs
+++ DotNet_Shoppable/Controllers/CartController.cs
@@ -70,9 +70,91 @@ namespace DotNet_Shoppable.Controllers
             return RedirectToAction("Confirm");
         }
 
+        //oConfirmed orders
+        public IActionResult Confirm()
+        {
 
+            List<OrderItem> cartItems = CartHelper.GetCartItems(Request, Response, db);
+            decimal total = CartHelper.GetSubTotal(cartItems) + shippingFee;
 
+            //calculate cart size
+            int cartSize = 0;
+            foreach (var item in cartItems)
+            {
+                cartSize += item.Quantity;
+            }
 
+            //read the delivery address and payment method
+            string deliveryAddress = TempData["DeliveryAddress"] as string ?? "";
+            string paymentMethod = TempData["PaymentMethod"] as string ?? "";
+            TempData.Keep(); // store the temp data of the payment info and delivery address for later use
+
+            if (cartSize == 0 || deliveryAddress.Length == 0 || paymentMethod.Length == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.DeliveryAddress = deliveryAddress;
+            ViewBag.PaymentMethod = paymentMethod;
+            ViewBag.Total = total;
+            ViewBag.CartSize = cartSize;
+
+            return View();
+        }
+
+        //Confirm orders
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Confirm(int any) // parameter will not be used
+        {
+            var cartItems = CartHelper.GetCartItems(Request, Response, db);
+
+            // read the data saved cache data from delivery address and payment entered
+            string deliveryAddress = TempData["DeliveryAddress"] as string ?? "";
+            string paymentMethod = TempData["PaymentMehtod"] as string ?? "";
+            TempData.Keep();
+
+            if (cartItems.Count == 0 || deliveryAddress.Length == 0 || paymentMethod.Length == 0 )
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // check if user is authenticated
+            var appUser = await userManager.GetUserAsync(User);
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            //save order
+            var order = new Order
+            {
+                ClientId = appUser.Id,
+                Items = cartItems,
+                ShippingFee = shippingFee,
+                DeliveryAddress = deliveryAddress,
+                PaymentMethod = paymentMethod,
+                PaymentStatus = "pending",
+                PaymentDetails = "",
+                OrderStatus = "pending",
+                //CreatedAt = DateTime.Now, // <=== uncomment when using sqlserver
+                CreatedAt = DateTime.UtcNow, // <=== comment out when using sqlserver
+            };
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            // delete the shopping cart cookie afterwards
+            Response.Cookies.Delete("shopping_cart");
+
+            ViewBag.SuccessMessage = "Order created successfully";
+
+            return View();
+
+        }
+
+       
+        
 
 
     }
